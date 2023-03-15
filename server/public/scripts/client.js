e2ebb156fcdac331bf5b87bdfafed7a857b76664
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-15 10:59:41 -0500

    me trying to get the delete function to work how i want.  not going very well..

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,7 +30,7 @@ $.ajax({
  console.log('in the GET number function', response);
  //Getting the recent response for answerRenderArea
  let thisResponse = response[response.length -1];//shifts to previous array, as far as I understand it..
- //same as array.shif();
+ //same as array.shift();
  $('#answerRenderArea').text();
  //empty the answerRenderArea
  $('#previousAnswersRenderArea').empty();
@@ -67,56 +67,11 @@ $.ajax({
 
 function deleteFunction(){
 console.log('trying to delete this ish');
+  $('#answerRenderArea').remove();
   $('#previousAnswersRenderArea').remove();
-
+getNumber();
 }
-// $.ajax({
-// method:'POST',
-// url: '/number',
-// data: {numOne: enteredNumbers.numberOneInput,
-//       numTwo: enteredNumbers.numberTwoInput,
-//       operator: '+',
-//       equal: '6'
-// } 
-// }).then(function(response){
-//   console.log('post is good');
-//   renderAnswer();
-// });
-// }//Ends equals function
 
-// function subtractInput(){
-// console.log('in subtractInput function');
-// //made a function that adds numbers together
-// enteredNumbers.operator = '-';
-// $.ajax({
-//   url: '-',
-//   mothod:'POST'
-// }).then((respons)=>{
-//   console.log('using subtractInput function', respons)});
-//  };
-// //Ends subtractInput function
-
-// function multiplyInput(){
-// console.log('in multiplyInput function');
-// //made a function that adds numbers together
-// enteredNumbers.operator = '*';
-// $.ajax({
-//   url:'/multiply',
-//   method: 'POST'
-// }).then((response)=>{
-//   console.log('Giving multiply POST')});
-//  };//Ends MultiplyInput function
-
-// function divideInput(){
-// console.log('in divideInput Function');
-// //made a function that adds numbers together
-// enteredNumbers.operator = '/';
-// $.ajax({
-//   url: 'divide',
-//   method: 'POST'
-// }).then((response)=>{
-//   console.log('using divide POST', response)});
-//  };//Ends divideInput Function
 
 
 
@@ -136,44 +91,5 @@ $('#answerRenderArea').append(`
 `)
 };
 };//end render function
-
-
-// Client-server client side functions:
-// 	listeners for buttons
-// 		ajax.POST +
-// 		ajax.POST -
-// 		ajax.POST *
-// 		ajax.POST /
-// 		ajax.POST =
-// 		ajax.POST C
-// 		ajax.POST Input Fields
-// 		ajax.GET render current answer
-// 		ajax.GET render all calculations
-
-// Client side functions:
-// 	startUp()
-// 	render()
-	
-
-// Client side global variables
-// 	??Client-Server server side functions:
-// 	'listener' for POST and GETs
-// 		POST +
-// 		POST -
-// 		POST *
-// 		POST /
-// 		POST =
-// 		POST C
-// 		GET renderCurrent
-// 		GET renderPastCalcs
-
-// Server side functions
-// 	variableChanger() - change a variable to +,-,*,/ per button pushed
-// 	runCalculation() - run actual calculation
-	
-// Server side global variables
-// 	currentOperator
-// 	currentAnswer
-// 	calculationArray
 
 //I have spent countless hours on this program to get things inputed.  I have used other expamples as well as other cohort members for guidance. 
